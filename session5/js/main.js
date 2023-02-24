c04ab099524ee0024dd5732d480c064f981076a5
mohamed amr <dev.mohamedamr@gmail.com>
2023-02-24 18:20:16 +0200

    z

---
+++ session5/js/main.js
@@ -0,0 +1,107 @@
+// alert('hi');
+
+var number = 12
+// alert(number);
+// console.log(number);
+// window.alert("ahne");
+// document.write("hi");
+// var ele = document.getElementById("tag").innerText = "ahmed"    
+// console.log(ele);
+// var age = prompt("enter ur age");
+
+// if(age > 20)
+// {
+//     console.log("happy birth day")
+// }else{
+//     console.log("good luck")
+// }
+
+
+
+// var password = 123456;
+
+// var userpassword = prompt("Enter your password")
+
+
+// if(password == userpassword){
+//     console.log("success")
+//     // document.getElementById("tag").innerText = "success"
+// }else{
+//     console.log("fail")
+//     // document.getElementById("tag").innerText = "fail"
+// }
+
+
+
+
+
+// mohamed => hi mohamed
+// ahmed => hi ahmed
+// eslam => hi eslam
+
+
+// var username = prompt("enter name")
+
+// if(username == "mohamed"){
+//     console.log("hi mohamed")
+// }else if(username == "eslam"){
+//     console.log("hi eslam")
+// }else if(username == "ahmed"){
+//     console.log("hi ahmed")
+// }else{
+//     console.log("not found")
+// }
+
+
+// console.log(10 * 20)
+
+
+
+// var oil = prompt("oil"); // 80
+// var l = prompt("li"); // 10
+
+// if(oil == 80){
+//     console.log(2 * l);
+// }else if(oil == 90){
+//     console.log(3 * l);
+// }else if(oil == 95){
+//     console.log(2 * l);
+// }else{
+//     console.log("not found")
+// }
+
+
+
+
+//  50 > deg  (fall)
+//  50 ----> 70 (good)
+// 71 -----> 80 ( v good)
+// 80 -----> 100 (exc)
+
+// if( deg > 50 && deg < 70 ){
+
+// }
+
+var deg = prompt("deg");
+
+if(deg <= 50){
+    console.log("fall")
+}else if (deg > 50 && deg <= 70){
+    console.log("good")
+}else if (deg > 70 && deg <= 80){
+    console.log("v good")
+}else if (deg > 80 && deg <= 100){
+    console.log("excellent ")
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ session6/js/main.js
@@ -0,0 +1,168 @@
+// var username = "mohamed";
+
+// switch(username){
+//     case "ahmed":
+//         console.log("hi ahmed")
+//     break; 
+//     case "mohamed":
+//         console.log("hi mohamed")
+//     break;
+//     case "mohamedamr":
+//         console.log("hi mohamedamr")
+//     break;
+//     default :
+//         console.log("error");           
+// }
+
+
+
+
+
+
+
+
+// var x = 10;
+// if(x == 10){
+//     console.log("10")
+// }else{
+//     console.log("error")
+// }
+
+
+
+// var x = 11;
+
+// var res = (x == 10) ? "10" : "error";
+
+// console.log(res);
+
+
+
+// var g = 0;
+// var gender = ( g == 1 ) ? "female" : "male";
+
+// console.log(gender);
+
+
+// var color = "red , blue";
+
+//              0    1       2
+var colors = ["red","green",11];
+
+// var i = 0;
+// while(colors.length>i){
+//     console.log(colors[i]);
+//     i++;
+// }
+
+
+
+// var i = 1;
+
+// while(21>i){
+//     if(i == 13){
+//         console.log("mohamed amr")
+//     }else{
+//         console.log(i);
+//     }
+    
+//     i++;
+// }
+
+
+
+// while
+// do while 
+// for
+
+
+// var i = 1;
+// i++;
+// console.log(i)
+// while(8>i){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// console.log(7%3);
+
+// 1
+// 16
+
+// 3 => fizz
+// 5 => buzz
+// 3,5 => fizzbuzz
+
+
+// var i = 1;
+// while(16>=i){
+//     if(i%5 == 0 && i%3 == 0){
+//         console.log("fizzbuzz");
+//     }else if(i%5 == 0){
+//         console.log("buzz");
+//     }
+//     else if(i%3 == 0){
+//         console.log("fizz");
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+
+// var numbers = [23,5,12,100,660,1];
+// var i = 0;
+// var max = numbers[0];
+// while(numbers.length>i){
+//     if(max>numbers[i]){
+//         max = numbers[i];
+//     }
+//     i++;
+// }
+
+// console.log(max);
+
+
+
+// twosum
+
+// var numbers = [1,3,4,5];
+// var target = 6;
+
+// var i =0;
+// while(numbers.length>i){
+//     var x = 0;
+//     while(numbers.length>x){
+//         if(numbers[i] + numbers[x] == target){
+//             console.log(numbers[i] , numbers[x]);
+//         }
+//         x++;
+//     }
+//     i++;
+// }
+
+[
+// var numbers = [1,,3,4,5];
+// var target = 6;
+// var i=0
+// while(numbers.length>i){
+//     if(number[i]+number[i+1]==[target]){
+//         console.log(number[i],number[i+1])
+//     }
+// }]
+// var i = 0;
+// while(numbers.length>i){
+//     if(numbers[i] + numbers[i+1] == target){
+//         console.log(numbers[i],numbers[i+1])
+//     }
+//     i++;
+// }
+
+
+// [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
