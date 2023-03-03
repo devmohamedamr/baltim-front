2338e3d51d91af92ab1e1eae6caa6bf9beb7a10c
mohamed amr <dev.mohamedamr@gmail.com>
2023-03-03 13:07:18 +0200

    7

---
+++ session7/js/main.js
@@ -0,0 +1,39 @@
+// for ()
+// {}
+
+// var i = 1;
+// while(10>=i){
+//     console.log(i)
+//     i++;
+// }
+
+
+
+// for(var i = 1 ; 10>i ; i++)
+// {
+//     console.log(i)
+// }
+
+
+
+
+// var colors  = [ "red" , "green" , "blue" ]
+// colors.pop()
+// console.log(colors)
+// while(x = colors.pop())
+// {
+//     console.log(x)
+// }
+
+
+
+// console.log(colors)
+
+// var i = 0;
+// do {
+//     i++;
+//     console.log(i)
+// }while(10<i)
+
+
+
